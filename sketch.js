9cd7be9319be60b747da83bea89b3091e313c5f5
Anais Payoute <apayou1 @lsu.edu>
2024-09-30 12:54:43 -0500

    Game

---
+++ sketch.js
@@ -0,0 +1,27 @@
+function setup() {
+  createCanvas(400, 400);
+  ey = 0;
+  ex = random(0, width)
+}
+
+let ey, ex;
+
+function draw() {
+  background(200);
+
+  fill(150, 255, 255)
+  square(mouseX, height-100, 50);
+  // line(mouseX, mouseY, pmouseX, pmouseY);
+  fill(255, 105, 180)
+  ellipse(ex, ey, 10);
+  ey = ey + 10;
+
+  if(ey > height) {
+    ey = 0;
+    ex = random(0, width)
+  }
+
+  if (ey > height - 100 && ex > mouseX+25 && ex < mouseX-25) {
+    print("hit")
+  }
+}
